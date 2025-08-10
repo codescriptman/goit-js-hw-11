575e2fdbdd33ee71c05f0d30cd55b62b41a98c1e
Azazello <donazazello1312@gmail.com>
2025-08-11 00:26:35 +0300

    input

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // import SimpleLightbox from 'simplelightbox';
 // import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery } from './js/pixabay-api';
 import { showLoader } from './js/render-functions';
@@ -7,19 +9,32 @@ import { hideLoader } from './js/render-functions';
 import { clearGallery } from './js/render-functions';
 
 export const form = document.querySelector('.form');
-let query = null;
+const input = document.querySelector('input[type=text]');
+const btn = document.querySelector('button[type="submit"]');
 
-form.addEventListener('input', e => {
-  query = String(e.target.value);
-  return query;
+btn.disabled = true;
+
+input.addEventListener('input', event => {
+  if (event.target.value === '') {
+    iziToast.error({
+      position: 'topRight',
+      message: `Fill empty fields`,
+    });
+  }
+  btn.disabled = false;
 });
 
 form.addEventListener('submit', event => {
   clearGallery();
   event.preventDefault();
+  let query = String(input.value);
   form.reset();
   if (query === '') {
-    event.target.disabled = true;
+    btn.disabled = true;
+    iziToast.error({
+      position: 'topRight',
+      message: `Fill empty fields`,
+    });
   }
   showLoader();
   getImagesByQuery(query);
